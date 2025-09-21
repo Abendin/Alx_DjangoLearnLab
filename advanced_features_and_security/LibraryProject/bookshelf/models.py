ccc35c6f16b8ad7ad6b18686cc0c5468fbdaa4b7
Abel Awenatei Abendin <abendinabel@gmail.com>
2025-09-21 23:45:20 +0000

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -38,3 +38,22 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+# NEW MODEL WITH CUSTOM PERMISSIONS
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
