6ba0d16b146e8a80b8a889bb2719d573e4e60f8b
Abel Awenatei Abendin <abendinabel@gmail.com>
2025-09-21 23:51:16 +0000

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,35 +1,22 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
-from .models import Book
+from django.shortcuts import render
+from .models import CustomUser
+from django import forms
 
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
+# Example search form to validate input
+class SearchForm(forms.Form):
+    query = forms.CharField(max_length=100, required=False)
 
-@permission_required('bookshelf.can_create', raise_exception=True)
-def book_create(request):
-    if request.method == "POST":
-        title = request.POST.get("title")
-        author = request.POST.get("author")
-        published_date = request.POST.get("published_date")
-        Book.objects.create(title=title, author=author, published_date=published_date, owner=request.user)
-        return redirect('book_list')
-    return render(request, 'bookshelf/book_form.html')
+def user_list(request):
+    """
+    Securely fetch users with optional search.
+    Avoids raw SQL by using Django ORM filtering.
+    """
+    form = SearchForm(request.GET or None)
+    users = CustomUser.objects.all()
 
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def book_edit(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == "POST":
-        book.title = request.POST.get("title")
-        book.author = request.POST.get("author")
-        book.published_date = request.POST.get("published_date")
-        book.save()
-        return redirect('book_list')
-    return render(request, 'bookshelf/book_form.html', {'book': book})
+    if form.is_valid():
+        query = form.cleaned_data.get("query")
+        if query:
+            users = users.filter(username__icontains=query)
 
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def book_delete(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    book.delete()
-    return redirect('book_list')
+    return render(request, "bookshelf/user_list.html", {"form": form, "users": users})
